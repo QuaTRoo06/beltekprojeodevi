3e119de58ae383770bb1c9da617d9e46bf5e047b
gokhanalagoz06@gmail.com <gokhanalagoz06@gmail.com>
2023-03-31 22:27:02 +0300

    ilk gönderim

---
+++ WebApplication2MVC/Models/Ogretmen.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2MVC.Models
+{
+    public class Ogretmen
+    {
+        public int Ogretmenid { get; set; }
+        public string AdSoyad { get; set; }
+        public string Brans { get; set; }
+    }
+}
---
+++ WebApplication2MVC/Models/OkulDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2MVC.Models
+
+
+{
+    public class OkulDbContext : DbContext
+    {
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        public DbSet<Ogretmen> Ogretmenler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbAyarlari)
+        {
+            base.OnConfiguring(dbAyarlari);
+            dbAyarlari.UseSqlServer("Data Source=.;Integrated Security=true;Initial Catalog=OkulDbMVC");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ogrenci>().ToTable("tblOgrenciler");
+            //modelBuilder.Entity<Ogrenci>().Property(o => o.OgrenciId).HasColumnType("varchar").HasMaxLength(30).IsRequired();
+            modelBuilder.Entity<Ogrenci>().Property(o => o.AdSoyad).HasColumnType("varchar").HasMaxLength(30).IsRequired();
+        }
+    }
+}
